087917cba38112c42a9ec2259fda5aff1c17cc4d
yazeed1993 <yazeed753@gmail.com>
2023-08-30 23:40:31 +0300

    solve todo 2

--- final_project.py
+++ final_project.py
@@ -3,12 +3,19 @@
 Name : Yazeed osama elderawy
 Delivery Date :
 """
-
+import uuid
 
 # TODO 2 Define Course Class and define constructor with
 # course_id (generated using uuid4) ,
 # course name (user_input) and
 # course mark (user_input)
+
+
+class Course:
+    def __init__(self, course_name, course_mark):
+        self.course_id = uuid.uuid4()
+        self.course_name = course_name
+        self.course_mark = course_mark
 
 
 class Student:
